9895ba8187652fc15c36ce1069e4ce66e47ca94d
babpur <namoonlvo@gmail.com>
2024-04-18 10:15:43 +0900

    [김지훈] dept table VO & emp table VO 생성

---
+++ src/main/java/vo/Dept.java
@@ -0,0 +1,12 @@
+package vo;
+
+// VO: Value Object > DTO: Date Transfer Object, Domain
+// ㄴ DTO는 VO에 속함
+// table 속성을 가지면 domain이라고 부를 수 있다.
+
+public class Dept {
+	public int depNo;
+	public String dName;
+	public String loc;
+
+}
